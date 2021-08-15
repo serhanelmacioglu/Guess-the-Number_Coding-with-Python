47f19553d43fa1c33e7384be5ecdc0e6ed9f35ce
Serhan Elmacıoğlu <70323782+serhanelmacioglu@users.noreply.github.com>
2021-08-15 18:25:21 +0300

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,7 +3,7 @@ number = random.randint(1, 10)
 
 player_name = input("Hello, what is your name? ")
 number_of_guesses = 0
-print('I\'m glad to meet you! ' + player_name+ ' \nLet\'s play a game with you, I will think a number between 1 and 10 then you will guess, alright? \nDon\'t forget! You have only 3 chances so guess:')
+print('I\'m glad to meet you! {} \nLet\'s play a game with you, I will think a number between 1 and 10 then you will guess, alright? \nDon\'t forget! You have only 3 chances so guess:'.format(player_name))
 
 while number_of_guesses < 3:
     guess = int(input())
@@ -15,6 +15,6 @@ while number_of_guesses < 3:
     if guess == number:
         break
 if guess == number:
-    print( 'Congratulations '+ player_name + ', ' + 'you guessed the number in ' + str(number_of_guesses) + ' tries!')
+    print( 'Congratulations {}, you guessed the number in {} tries!'.format(player_name, number_of_guesses))
 else:
-    print('Close but no cigar, you couldn\'t guess the number. \nWell, the number was ' + str(number) + '.')
+    print('Close but no cigar, you couldn\'t guess the number. \nWell, the number was {}.'.format(number))
